ad2217037a771eea009e570b8d3aeb78cd2275f2
TienThinh-fe <tienthinhhoang317@gmail.com>
2024-06-01 23:38:40 +0700

    fix: cors origin

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ const app = express();
 
 app.use(
   cors({
-    credentials: true,
-    origin: ['*'],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (['http://localhost:5173'].indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   }),
 );
 app.use(bodyParser.json());
